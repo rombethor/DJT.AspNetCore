a54b6ee97a45936a39c8c9bb7a2e916a27a06112
Daniel Thorne <thorne.danielj@gmail.com>
2022-07-25 12:38:58 -0500

    Added useful extension method.

---
+++ DJT.AspNetCore/HttpResponseMessageExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DJT.AspNetCore.Mvc;
+
+namespace DJT.AspNetCore
+{
+    /// <summary>
+    /// Extension methods for HttpResponseMessage
+    /// </summary>
+    public static class HttpResponseMessageExtensions
+    {
+        /// <summary>
+        /// Convert HttpResponseMessage to general implementation of IActionResult
+        /// for controller action responses.
+        /// </summary>
+        /// <param name="msg">The HttpResponseMessage to implement in the response</param>
+        /// <returns>General implementation of IActionResult</returns>
+        public static HttpResponseMessageResult ToActionResult(this HttpResponseMessage msg)
+        {
+            return new HttpResponseMessageResult(msg);
+        }
+    }
+}
